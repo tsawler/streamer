2f1c37fac8342d82c16c893d6cf94fdb22422d00
Trevor Sawler <trevor.sawler@gmail.com>
2024-03-11 15:22:39 -0300

    hls conversion

--- streamer.go
+++ streamer.go
@@ -3,7 +3,10 @@ package streamer
 import (
 	"fmt"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 type Encoder struct {
@@ -20,24 +23,50 @@ func New(in, out string, seg int) *Encoder {
 	}
 }
 
-func (e *Encoder) Encode() error {
-	// Create the HLS playlist and segment the video using ffmpeg
+func (e *Encoder) Encode() (string, error) {
+	b := path.Base(e.InputFile)
+	baseFileName := strings.TrimSuffix(b, filepath.Ext(b))
+
 	ffmpegCmd := exec.Command(
 		"ffmpeg",
 		"-i", e.InputFile,
+		"-map", "0:v:0",
+		"-map", "0:a:0",
+		"-map", "0:v:0",
+		"-map", "0:a:0",
+		"-map", "0:v:0",
+		"-map", "0:a:0",
+		"-c:v", "libx264",
+		"-crf", "22",
+		"-c:a", "aac",
+		"-ar", "48000",
+		"-filter:v:0", "scale=w=480:h=270",
+		"-maxrate:v:0", "600k",
+		"-b:a:0", "64k",
+		"-filter:v:1", "scale=w=640:h=360",
+		"-maxrate:v:1", "900k",
+		"-b:a:1", "128k",
+		"-filter:v:2", "scale=w=1280:h=720",
+		"-maxrate:v:2", "600k",
+		"-b:a:2", "64k",
+		"-var_stream_map", "v:0,a:0,name:360p v:1,a:1,name:480p v:2,a:2,name:720p",
+		"-preset", "slow",
+		"-hls_list_size", "0",
+		"-threads", "0",
+		"-f", "hls",
+		"-hls_playlist_type", "event",
+		"-hls_time", strconv.Itoa(e.SegmentDuration),
+		"-hls_flags", "independent_segments",
+		"-master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
 		"-profile:v", "baseline", // baseline profile is compatible with most devices
 		"-level", "3.0",
-		"-start_number", "0", // start numbering segments from 0
-		"-hls_time", strconv.Itoa(e.SegmentDuration), // duration of each segment in seconds
-		"-hls_list_size", "0", // keep all segments in the playlist
-		"-f", "hls",
-		fmt.Sprintf("%s/playlist.m3u8", e.OutputDir),
+		fmt.Sprintf("%s/%s-%%v.m3u8", e.OutputDir, baseFileName),
 	)
 
 	output, err := ffmpegCmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("failed to create HLS: %v\nOutput: %s", err, string(output))
+		return "", fmt.Errorf("failed to create HLS: %v\nOutput: %s", err, string(output))
 	}
 
-	return nil
+	return fmt.Sprintf("%s/%s.m3u8", e.OutputDir, baseFileName), nil
 }
