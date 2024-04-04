bb9ad075f67c28f9710d4e4f03730342596569d9
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-04 17:13:28 -0300

    clean up

---
+++ encoders.go
@@ -0,0 +1,190 @@
+package streamer
+
+import (
+	"fmt"
+	"github.com/xfrr/goffmpeg/transcoder"
+	"log"
+	"os/exec"
+	"strconv"
+)
+
+// Encoder is an interface for encoding video. Any type that wants to satisfy
+// this interface must implement all its methods.
+type Encoder interface {
+	EncodeToMP4(v *Video, baseFileName string) error
+	EncodeToHLS(v *Video) error
+	EncodeToHLSEncrypted(v *Video)
+}
+
+// VideoEncoder is a type which satisfies the Encoder interface.
+type VideoEncoder struct{}
+
+// EncodeToMP4 takes a Video object and a base file name, and encodes to MP4 format.
+func (ve *VideoEncoder) EncodeToMP4(v *Video, baseFileName string) error {
+	trans := new(transcoder.Transcoder)
+
+	outputPath := fmt.Sprintf("%s/%s.mp4", v.OutputDir, baseFileName)
+
+	err := trans.Initialize(v.InputFile, outputPath)
+	if err != nil {
+		return err
+	}
+
+	// set codec
+	trans.MediaFile().SetVideoCodec("libx264")
+
+	// Start transcoder process with progress checking
+	done := trans.Run(true)
+
+	go func() {
+		// Returns a channel to get the transcoding progress
+		progress := trans.Output()
+
+		// Printing transcoding progress to log
+		curProgress := 0
+		oldInt := 0
+
+		for msg := range progress {
+			if int(msg.Progress)%2 == 0 {
+				if oldInt != int(msg.Progress) {
+					// we have moved up 2%
+					curProgress = curProgress + 2
+					oldInt = int(msg.Progress)
+					log.Printf("%d: %d%%\n", v.ID, curProgress)
+
+					data := map[string]string{
+						"message":  "progress",
+						"video_id": fmt.Sprintf("%d", v.ID),
+						"percent":  fmt.Sprintf("%d", int(msg.Progress)),
+					}
+					v.pushJSONToWs(data)
+					v.pushToWs(fmt.Sprintf("%d", int(msg.Progress)))
+				}
+			}
+		}
+	}()
+
+	// This channel is used to wait for the transcoding process to end
+	err = <-done
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// EncodeToHLS takes a Video object and a base file name, and encodes to HLS format.
+func (ve *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
+	result := make(chan error)
+
+	go func(result chan error) {
+		ffmpegCmd := exec.Command(
+			"ffmpeg",
+			"-i", v.InputFile,
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-c:v", "libx264",
+			"-crf", "22",
+			"-c:a", "aac",
+			"-ar", "48000",
+			"-filter:v:0", "scale=-2:1080",
+			"-maxrate:v:0", v.MaxRate1080p,
+			"-b:a:0", "128k",
+			"-filter:v:1", "scale=-2:720",
+			"-maxrate:v:1", v.MaxRate720p,
+			"-b:a:1", "128k",
+			"-filter:v:2", "scale=-2:480",
+			"-maxrate:v:2", v.MaxRate480p,
+			"-b:a:2", "64k",
+			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p",
+			"-preset", "slow",
+			"-hls_list_size", "0",
+			"-threads", "0",
+			"-f", "hls",
+			"-hls_playlist_type", "event",
+			"-hls_time", strconv.Itoa(v.SegmentDuration),
+			"-hls_flags", "independent_segments",
+			"-hls_segment_type", "mpegts",
+			"-hls_playlist_type", "vod",
+			"-master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
+			"-profile:v", "baseline", // baseline profile is compatible with most devices
+			"-level", "3.0",
+			"-progress", "-",
+			"-nostats",
+			fmt.Sprintf("%s/%s-%%v.m3u8", v.OutputDir, baseFileName),
+		)
+
+		_, err := ffmpegCmd.CombinedOutput()
+		result <- err
+	}(result)
+
+	err := <-result
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// EncodeToHLSEncrypted takes a Video object and a base file name, and encodes to encrypted HLS format.
+func (ve *VideoEncoder) EncodeToHLSEncrypted(v *Video, baseFileName string) error {
+	result := make(chan error)
+
+	go func(result chan error) {
+		ffmpegCmd := exec.Command(
+			"ffmpeg",
+			"-i", v.InputFile,
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-c:v", "libx264",
+			"-crf", "22",
+			"-c:a", "aac",
+			"-ar", "48000",
+			"-filter:v:0", "scale=-2:1080",
+			"-maxrate:v:0", v.MaxRate1080p,
+			"-b:a:0", "128k",
+			"-filter:v:1", "scale=-2:720",
+			"-maxrate:v:1", v.MaxRate720p,
+			"-b:a:1", "128k",
+			"-filter:v:2", "scale=-2:480",
+			"-maxrate:v:2", v.MaxRate480p,
+			"-b:a:2", "64k",
+			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p",
+			"-preset", "slow",
+			"-hls_list_size", "0",
+			"-threads", "0",
+			"-f", "hls",
+			"-hls_playlist_type", "event",
+			"-hls_time", strconv.Itoa(v.SegmentDuration),
+			"-hls_flags", "independent_segments",
+			"-hls_segment_type", "mpegts",
+			"-hls_key_info_file", v.KeyInfo,
+			"-hls_playlist_type", "vod",
+			"-master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
+			"-profile:v", "baseline", // baseline profile is compatible with most devices
+			"-level", "3.0",
+			"-progress", "-",
+			"-nostats",
+			fmt.Sprintf("%s/%s-%%v.m3u8", v.OutputDir, baseFileName),
+		)
+
+		_, err := ffmpegCmd.CombinedOutput()
+		result <- err
+	}(result)
+
+	err := <-result
+	if err != nil {
+		log.Println("Error encoding encrypted:", err.Error())
+		return err
+	}
+
+	return nil
+}
