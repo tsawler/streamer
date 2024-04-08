ebc611138d5a35a4156fdb7d040570a848f8a9c0
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-08 17:25:39 -0300

    improve tests

--- streamer_test.go
+++ streamer_test.go
@@ -130,25 +130,34 @@ func Test_encode(t *testing.T) {
 		ops *VideoOptions
 	}
 	tests := []struct {
-		name          string
-		args          args
-		expectSuccess bool
+		name           string
+		args           args
+		specifyEncoder bool
+		expectSuccess  bool
 	}{
-		{name: "mp4", args: args{1, "mp4", &VideoOptions{RenameOutput: false}}, expectSuccess: true},
-		{name: "hls rename", args: args{2, "hls", &VideoOptions{RenameOutput: true}}, expectSuccess: true},
-		{name: "hls encrypted", args: args{3, "hls-encrypted", &VideoOptions{RenameOutput: true}}, expectSuccess: true},
-		{name: "expect error", args: args{4, "fish", &VideoOptions{RenameOutput: true}}, expectSuccess: false},
+		{name: "mp4", args: args{1, "mp4", &VideoOptions{RenameOutput: false}}, expectSuccess: true, specifyEncoder: false},
+		{name: "mp4_fail", args: args{2, "mp4", &VideoOptions{RenameOutput: false}}, expectSuccess: false, specifyEncoder: true},
+		{name: "hls rename", args: args{3, "hls", &VideoOptions{RenameOutput: true}}, expectSuccess: true, specifyEncoder: false},
+		{name: "hls_fail", args: args{4, "hls", &VideoOptions{RenameOutput: true}}, expectSuccess: false, specifyEncoder: true},
+		{name: "hls encrypted", args: args{5, "hls-encrypted", &VideoOptions{RenameOutput: false}}, expectSuccess: true, specifyEncoder: false},
+		{name: "hls encrypted rename", args: args{5, "hls-encrypted", &VideoOptions{RenameOutput: true}}, expectSuccess: true, specifyEncoder: false},
+		{name: "hls encrypted_fail", args: args{5, "hls-encrypted", &VideoOptions{RenameOutput: true}}, expectSuccess: false, specifyEncoder: true},
+		{name: "expect error", args: args{6, "fish", &VideoOptions{RenameOutput: true}}, expectSuccess: false},
 	}
 	for _, tt := range tests {
 		wp := New(make(chan VideoProcessingJob), 1)
 		v := wp.NewVideo(tt.args.id, "./testdata/i.mp4", "./testdata/output", tt.args.enc, testNotifyChan, tt.args.ops)
-		v.Encoder = testProcessor
+		if tt.specifyEncoder {
+			v.Encoder = testProcessorFailing
+		} else {
+			v.Encoder = testProcessor
+		}
 
 		v.encode()
 
 		result := <-testNotifyChan
 		if result.Successful != tt.expectSuccess {
-			t.Errorf("%s: encoding failed: %s", tt.name, result.Message)
+			t.Errorf("%s: expected result.Successful of %t but got %t", tt.name, result.Successful, tt.expectSuccess)
 		}
 	}
 }
