024436502055bceceeed07df4ff60d0bcdcb5477
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-08 14:56:08 -0300

    testing

--- streamer_test.go
+++ streamer_test.go
@@ -46,7 +46,9 @@ func TestNewVideo(t *testing.T) {
 		args args
 	}{
 		{name: "mp4", args: args{1, "mp4", make(chan ProcessingMessage), &VideoOptions{RenameOutput: false}}},
-		{name: "hls", args: args{1, "hls", make(chan ProcessingMessage), &VideoOptions{RenameOutput: true}}},
+		{name: "mp4 rename", args: args{1, "mp4", make(chan ProcessingMessage), &VideoOptions{RenameOutput: true}}},
+		{name: "hls", args: args{1, "hls", make(chan ProcessingMessage), &VideoOptions{RenameOutput: false}}},
+		{name: "hls rename", args: args{1, "hls", make(chan ProcessingMessage), &VideoOptions{RenameOutput: true}}},
 		{name: "mp4 empty", args: args{1, "", make(chan ProcessingMessage), &VideoOptions{RenameOutput: false}}},
 	}
 
@@ -62,18 +64,68 @@ func TestNewVideo(t *testing.T) {
 }
 
 func Test_encodeToMP4(t *testing.T) {
-	wp := New(make(chan VideoProcessingJob), 1)
-	v := wp.NewVideo(1, "./testdata/i.mp4", "./testdata/output", "mp4", testNotifyChan, nil)
-	v.Encoder = testProcessor
-
-	err := v.encodeToMP4()
-	if err != nil {
-		t.Error("encode to mp4 failed", err)
-		return
+	type args struct {
+		id  int
+		enc string
+		ops *VideoOptions
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "mp4", args: args{1, "mp4", &VideoOptions{RenameOutput: false}}},
+		{name: "mp4 rename", args: args{2, "mp4", &VideoOptions{RenameOutput: true}}},
+		{name: "mp4 no ops", args: args{2, "mp4", nil}},
 	}
 
-	result := <-testNotifyChan
-	if !result.Successful {
-		t.Error("encoding failed")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wp := New(make(chan VideoProcessingJob), 1)
+			v := wp.NewVideo(tt.args.id, "./testdata/i.mp4", "./testdata/output", "mp4", testNotifyChan, tt.args.ops)
+			v.Encoder = testProcessor
+
+			err := v.encodeToMP4()
+			if err != nil {
+				t.Errorf("%s: encode to mp4 failed: %s", tt.name, err.Error())
+				return
+			}
+
+			result := <-testNotifyChan
+			if !result.Successful {
+				t.Errorf("%s: encoding failed", tt.name)
+			}
+		})
 	}
+
+}
+
+func Test_encode(t *testing.T) {
+	type args struct {
+		id  int
+		enc string
+		ops *VideoOptions
+	}
+	tests := []struct {
+		name          string
+		args          args
+		expectSuccess bool
+	}{
+		{name: "mp4", args: args{1, "mp4", &VideoOptions{RenameOutput: false}}, expectSuccess: true},
+		{name: "hls rename", args: args{2, "hls", &VideoOptions{RenameOutput: true}}, expectSuccess: true},
+		{name: "hls encrypted", args: args{3, "hls-encrypted", &VideoOptions{RenameOutput: true}}, expectSuccess: true},
+		{name: "expect error", args: args{4, "fish", &VideoOptions{RenameOutput: true}}, expectSuccess: false},
+	}
+	for _, tt := range tests {
+		wp := New(make(chan VideoProcessingJob), 1)
+		v := wp.NewVideo(tt.args.id, "./testdata/i.mp4", "./testdata/output", tt.args.enc, testNotifyChan, tt.args.ops)
+		v.Encoder = testProcessor
+
+		v.encode()
+
+		result := <-testNotifyChan
+		if result.Successful != tt.expectSuccess {
+			t.Errorf("%s: encoding failed: %s", tt.name, result.Message)
+		}
+	}
+
 }
