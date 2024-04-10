a9ff3e8b0008620359c652810072135114fa42f4
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-10 09:08:09 -0300

    tests

--- encoders_test.go
+++ encoders_test.go
@@ -7,17 +7,24 @@ func Test_All_Encoders(t *testing.T) {
 		id   int
 		file string
 		enc  string
+		ops  *VideoOptions
 	}
 	tests := []struct {
 		name          string
 		expectSuccess bool
 		args          args
 	}{
-		{name: "mp4 error", expectSuccess: false, args: args{id: 1, file: "a.mp4", enc: "mp4"}},
-		{name: "hls error", expectSuccess: false, args: args{id: 2, file: "a.mp4", enc: "hls"}},
-		{name: "hls-encrypted error", expectSuccess: false, args: args{id: 3, file: "a.mp4", enc: "hls-encrypted"}},
-		{name: "mp4", expectSuccess: true, args: args{id: 4, file: "./testdata/dog.mp4", enc: "mp4"}},
-		{name: "hls", expectSuccess: true, args: args{id: 5, file: "./testdata/dog.mp4", enc: "hls"}},
+		{name: "mp4 error", expectSuccess: false, args: args{id: 1, file: "a.mp4", enc: "mp4", ops: nil}},
+		{name: "hls error", expectSuccess: false, args: args{id: 2, file: "a.mp4", enc: "hls", ops: nil}},
+		{name: "hls-encrypted error", expectSuccess: false, args: args{id: 3, file: "a.mp4", enc: "hls-encrypted", ops: nil}},
+		{name: "mp4", expectSuccess: true, args: args{id: 4, file: "./testdata/dog.mp4", enc: "mp4", ops: nil}},
+		{name: "hls", expectSuccess: true, args: args{id: 5, file: "./testdata/dog.mp4", enc: "hls", ops: nil}},
+		{name: "hls-encrypted", expectSuccess: true, args: args{id: 6, file: "./testdata/dog.mp4", enc: "hls",
+			ops: &VideoOptions{
+				RenameOutput: false,
+				Secret:       "enc.key",
+				KeyInfo:      "./testdata/keys/enc.keyinfo",
+			}}},
 	}
 
 	for _, tt := range tests {
