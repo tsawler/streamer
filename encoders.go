557d72619974dd4adac90ca01cdfff04f5f5cc79
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-16 10:47:25 -0300

    comments

--- encoders.go
+++ encoders.go
@@ -56,26 +56,26 @@ func (ve *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
 		ffmpegCmd := exec.Command(
 			"ffmpeg",
 			"-i", v.InputFile,
+			"-map", "0:v:0", // We need three of this line and the next (one
+			"-map", "0:a:0", // for each of the resolutions we want to encode to).
 			"-map", "0:v:0",
 			"-map", "0:a:0",
 			"-map", "0:v:0",
 			"-map", "0:a:0",
-			"-map", "0:v:0",
-			"-map", "0:a:0",
-			"-c:v", "libx264",
+			"-c:v", "libx264", // Our video codec (H.264/MPEG-4 AVC video coding format).
 			"-crf", "22",
 			"-c:a", "aac",
 			"-ar", "48000",
-			"-filter:v:0", "scale=-2:1080",
+			"-filter:v:0", "scale=-2:1080", // 1080p resolution.
 			"-maxrate:v:0", v.Options.MaxRate1080p,
 			"-b:a:0", "128k",
-			"-filter:v:1", "scale=-2:720",
+			"-filter:v:1", "scale=-2:720", // 720p resolution.
 			"-maxrate:v:1", v.Options.MaxRate720p,
 			"-b:a:1", "128k",
-			"-filter:v:2", "scale=-2:480",
+			"-filter:v:2", "scale=-2:480", // 480p resolution.
 			"-maxrate:v:2", v.Options.MaxRate480p,
 			"-b:a:2", "64k",
-			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p",
+			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name:720p v:2,a:2,name:480p", // Our map of resolutions.
 			"-preset", "slow",
 			"-hls_list_size", "0",
 			"-threads", "0",
@@ -86,7 +86,7 @@ func (ve *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
 			"-hls_segment_type", "mpegts",
 			"-hls_playlist_type", "vod",
 			"-master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
-			"-profile:v", "baseline", // baseline profile is compatible with most devices
+			"-profile:v", "baseline", // The baseline profile is compatible with most devices
 			"-level", "3.0",
 			"-progress", "-",
 			"-nostats",
